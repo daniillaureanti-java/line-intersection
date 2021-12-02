ee96ac20adcf446078c61bd8591181a4a2259fbd
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-02 17:51:58 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,24 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
+    private final int k;
+    private final int b;
 
     public Line(int k, int b) {
-
+        this.k = k;
+        this.b = b;
     }
 
+
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        int deltaB = this.b - other.b;
+        int deltaK = other.k - this.k;
+        if (deltaK == 0)
+            return null;
+        else {
+            int x = deltaB/deltaK;
+            return new Point(x, this.k*x + this.b );
+        }
     }
 
 }
